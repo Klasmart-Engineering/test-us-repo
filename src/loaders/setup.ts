Klasmart-Engineering/user-service

Merged in UD-1266-create-usernode-query (pull request #580)
feat(UD-1266): Create usernode query

* adding missing scope arg to the dataloader test

* avoiding creating new dataloader per request

* turning loadNodeDataLoader into a generic func

* extracting userConnection expect func to utils

* acceptance test added to check there s only 1 db conn

* acceptance 1db conn test removed and added as integration

* fix: merge conflict solved

* fix: get rid of loadNodeDataLoader func

* fix: removing unnecesary import

* fix: adding missing import


Approved-by: Marlon Torres
Approved-by: Oliver Harrison

src/loaders/setup.ts
@@ -1,4 +1,10 @@
-import { IUsersConnectionLoaders } from './usersConnection'
+import Dataloader from 'dataloader'
+import {
+    IUsersConnectionLoaders,
+    orgsForUsers,
+    rolesForUsers,
+    schoolsForUsers,
+} from './usersConnection'
 import { IProgramsConnectionLoaders } from './programsConnection'
 import { IGradesConnectionLoaders } from './gradesConnection'
 import {
@@ -16,6 +22,8 @@ import {
 import { ISubjectsConnectionLoaders } from './subjectsConnection'
 import { IOrganizationsConnectionLoaders } from './organizationsConnection'
 import { ISchoolLoaders, organizationsForSchools, schoolsByIds } from './school'
+import { NodeDataLoader } from './genericNode'
+import { CoreUserConnectionNode } from '../pagination/usersConnection'
 import DataLoader from 'dataloader'
 import { User } from '../entities/user'
 import { Lazy } from '../utils/lazyLoading'
@@ -25,6 +33,10 @@ import { BrandingResult } from '../types/graphQL/branding'
 import { Organization } from '../entities/organization'
 import { School } from '../entities/school'
 
+interface IUserNodeDataLoaders extends Required<IUsersConnectionLoaders> {
+    node?: NodeDataLoader<User, CoreUserConnectionNode>
+}
+
 export interface IDataLoaders {
     usersConnection?: IUsersConnectionLoaders
     programsConnection?: IProgramsConnectionLoaders
@@ -33,6 +45,7 @@ export interface IDataLoaders {
     subjectsConnection?: ISubjectsConnectionLoaders
     organizationsConnection?: IOrganizationsConnectionLoaders
     user: IUsersLoaders
+    userNode: IUserNodeDataLoaders
     organization: IOrganizationLoaders
     school: ISchoolLoaders
 }
@@ -65,6 +78,11 @@ export function createContextLazyLoaders(): IDataLoaders {
             schoolById: new Lazy<DataLoader<string, School | undefined>>(
                 () => new DataLoader(schoolsByIds)
             ),
+        },
+        userNode: {
+            organizations: new Dataloader((keys) => orgsForUsers(keys)),
+            schools: new Dataloader((keys) => schoolsForUsers(keys)),
+            roles: new Dataloader((keys) => rolesForUsers(keys)),
         },
     }
 }

Klasmart-Engineering/test-us-repo

src/loaders/setup.ts
@@ -24,6 +24,7 @@ import {
     IChildConnectionDataloaderKey,
 } from './childConnectionLoader'
 import { IClassesConnectionLoaders } from './classesConnection'
+import { NodeDataLoader } from './genericNode'
 import { IGradesConnectionLoaders } from './gradesConnection'
 import {
     brandingForOrganizations,
@@ -40,7 +41,16 @@ import {
     schoolMembershipsForUsers,
     usersByIds,
 } from './user'
-import { IUsersConnectionLoaders } from './usersConnection'
+import {
+    IUsersConnectionLoaders,
+    orgsForUsers,
+    rolesForUsers,
+    schoolsForUsers,
+} from './usersConnection'
+
+interface IUserNodeDataLoaders extends Required<IUsersConnectionLoaders> {
+    node?: NodeDataLoader<User, CoreUserConnectionNode>
+}
 
 export interface IDataLoaders {
     usersConnection?: IUsersConnectionLoaders
@@ -50,6 +60,7 @@ export interface IDataLoaders {
     subjectsConnection?: ISubjectsConnectionLoaders
     organizationsConnection?: IOrganizationsConnectionLoaders
     user: IUsersLoaders
+    userNode: IUserNodeDataLoaders
     organization: IOrganizationLoaders
     school: ISchoolLoaders
 
@@ -118,5 +129,10 @@ export function createContextLazyLoaders(): IDataLoaders {
                     )
                 })
         ),
+        userNode: {
+            organizations: new DataLoader((keys) => orgsForUsers(keys)),
+            schools: new DataLoader((keys) => schoolsForUsers(keys)),
+            roles: new DataLoader((keys) => rolesForUsers(keys)),
+        },
     }
 }